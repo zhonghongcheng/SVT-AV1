5700297e1302818a7fc163fdd5fb5a732ddd4fb0
htmar <hassene.tmar@intel.com>
2019-07-28 19:38:30 -0700

    Bring back the interpolation after addressing the signaling issue

--- Source/Lib/Common/Codec/EbInterPrediction.c
+++ Source/Lib/Common/Codec/EbInterPrediction.c
@@ -7193,6 +7193,11 @@ EbErrorType inter_pu_prediction_av1(
 #if !PRE_BILINEAR_CLEAN_UP
         candidate_buffer_ptr->candidate_ptr->interp_filters = 0;
 #endif
+#if SET_1
+        if (picture_control_set_ptr->parent_pcs_ptr->interpolation_search_level == IT_SEARCH_OFF)
+            candidate_buffer_ptr->candidate_ptr->interp_filters = 0;
+        else
+#endif
         if (!md_context_ptr->skip_interpolation_search) {
             if (md_context_ptr->blk_geom->bwidth > capped_size && md_context_ptr->blk_geom->bheight > capped_size)
                 interpolation_filter_search(
