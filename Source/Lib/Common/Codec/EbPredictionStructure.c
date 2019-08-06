3adbc5a4686320b34251d095b19b4f4f00e1f78b
htmar <hassene.tmar@intel.com>
2019-08-06 01:08:03 -0700

    add mrp_mode check

--- Source/Lib/Common/Codec/EbPredictionStructure.c
+++ Source/Lib/Common/Codec/EbPredictionStructure.c
@@ -1791,9 +1791,11 @@ EbErrorType prediction_structure_group_ctor(
     PredictionStructureGroup *predictionStructureGroupPtr;
     EB_MALLOC(PredictionStructureGroup*, predictionStructureGroupPtr, sizeof(PredictionStructureGroup), EB_N_PTR);
     *predictionStructureGroupDblPtr = predictionStructureGroupPtr;
-
+#if M4_SET_MRP
+    uint8_t ref_count_used =  1;
+#else
     uint8_t ref_count_used = enc_mode <= ENC_M1 ? MAX_REF_IDX : enc_mode <= ENC_M3 ? 2 : 1;
-
+#endif
 #if MRP_M1
     if (ref_count_used > 0 && ref_count_used < MAX_REF_IDX) {
         for (int gop_i = 1; gop_i < 8; ++gop_i) {
