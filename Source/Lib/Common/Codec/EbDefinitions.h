01cb30fbf4b95813c9bbbde441b33c77b0992408
hguermaz <hassen.guermazi@intel.com>
2019-10-15 11:05:27 -0700

    upgrade FIX_ALTREF

--- Source/Lib/Common/Codec/EbDefinitions.h
+++ Source/Lib/Common/Codec/EbDefinitions.h
@@ -40,8 +40,8 @@ extern "C" {
 #define GROUP_2                                 0
 #if DEBUG_TOOLS
 #define QPS_QPM_OFF                             0
-#define ALT_REF_OFF                             1
-#define NSQ_OFF                                 0
+#define ALT_REF_OFF                             0
+#define NSQ_OFF                                 1
 #define TX_TYPE_OFF                             0
 #define ATB_OFF                                 0
 #define SHUT_FILTERING                          0 // CDEF RESTORATION DLF
