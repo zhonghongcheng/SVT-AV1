969c84cd4d16ce8022be2a2a5c876d190af2d756
hguermaz <hassen.guermazi@intel.com>
2019-11-08 20:59:08 -0800

    upgraded M4_TO_M5

--- Source/Lib/Common/Codec/EbEncDecProcess.c
+++ Source/Lib/Common/Codec/EbEncDecProcess.c
@@ -1739,8 +1739,10 @@ EbErrorType signal_derivation_enc_dec_kernel_oq(
 #if STAGE_1_COUNT_PRUNING_TH_S
     if (MR_MODE || picture_control_set_ptr->enc_mode == ENC_M0 || sequence_control_set_ptr->input_resolution == INPUT_SIZE_576p_RANGE_OR_LOWER)
         context_ptr->md_stage_1_count_th_s = (uint64_t)~0;
-    else
+    else if (picture_control_set_ptr->enc_mode <= ENC_M4)
         context_ptr->md_stage_1_count_th_s = 75;
+    else // to be tested for m5-m8
+        context_ptr->md_stage_1_count_th_s = (uint64_t)~0;
 #else
     if (MR_MODE)
         context_ptr->dist_base_md_stage_0_count_th = (uint64_t)~0;
@@ -1748,15 +1750,21 @@ EbErrorType signal_derivation_enc_dec_kernel_oq(
         context_ptr->dist_base_md_stage_0_count_th = 75;
 #endif
 #endif
-
+#if M5_MD_STAGING_1_COUNT_TH_S
+    context_ptr->md_stage_1_count_th_s = (uint64_t)~0;
+#endif
 #if STAGE_1_COUNT_PRUNING_TH_C
     // TH_C(for class removal)
     // Remove class if deviation to the best higher than TH_C
     if (MR_MODE || picture_control_set_ptr->enc_mode == ENC_M0 || sequence_control_set_ptr->input_resolution == INPUT_SIZE_576p_RANGE_OR_LOWER)
         context_ptr->md_stage_1_count_th_c = (uint64_t)~0;
-    else
+    else if (picture_control_set_ptr->enc_mode <= ENC_M4)
         context_ptr->md_stage_1_count_th_c = 100;
-
+    else // to be tested for m5-m8
+        context_ptr->md_stage_1_count_th_c = (uint64_t)~0;
+#endif
+#if M5_MD_STAGING_1_COUNT_TH_C
+    context_ptr->md_stage_1_count_th_c = (uint64_t)~0;
 #endif
 #if STAGE_2_COUNT_PRUNING_TH_S
     // TH_S(for candidate removal per class)
@@ -1786,7 +1794,7 @@ EbErrorType signal_derivation_enc_dec_kernel_oq(
     else
         context_ptr->md_stage_2_count_th_s = (uint64_t)~0; // until tested
 #endif
-#if M5_MD_STAGING_2_COUNT_TH
+#if M5_MD_STAGING_2_COUNT_TH_S
     context_ptr->md_stage_2_count_th_s = (uint64_t)~0;
 #endif
 #if STAGE_2_COUNT_PRUNING_TH_C
@@ -1800,7 +1808,7 @@ EbErrorType signal_derivation_enc_dec_kernel_oq(
         context_ptr->md_stage_2_count_th_c = (uint64_t)~0;
 
 #endif
-#if M5_MD_STAGING_2_COUNT_TH
+#if M5_MD_STAGING_2_COUNT_TH_C
     context_ptr->md_stage_2_count_th_c = (uint64_t)~0;
 #endif
 #if LESS_RECTANGULAR_CHECK_LEVEL
