e59c897f7af7b9f4e484a2b0273e254d9492a501
Omran <omran.abdelkafi@intel.com>
2019-11-21 12:05:18 -0800

    fixed M0_CANDIDATE_SC

--- Source/Lib/Encoder/Codec/EbResourceCoordinationProcess.c
+++ Source/Lib/Encoder/Codec/EbResourceCoordinationProcess.c
@@ -701,7 +701,7 @@ void* resource_coordination_kernel(void *input_ptr)
             sequence_control_set_ptr->seq_header.enable_interintra_compound =  (sequence_control_set_ptr->static_config.encoder_bit_depth == EB_10BIT &&
                                                                                 sequence_control_set_ptr->static_config.enable_hbd_mode_decision ) ? 0:
 #if M0_CANDIDATE_SC
-                                                                                (sequence_control_set_ptr->static_config.enc_mode == ENC_M0 && sequence_control_set_ptr->static_config.screen_content_mode == 0) ? 1 : 0;
+                                                                                (sequence_control_set_ptr->static_config.enc_mode == ENC_M0 && sequence_control_set_ptr->static_config.screen_content_mode != 1) ? 1 : 0;
 #else
                                                                                 (sequence_control_set_ptr->static_config.enc_mode == ENC_M0) ? 1 : 0;
 #endif
