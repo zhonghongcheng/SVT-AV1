5b650e64e18338816f9158e4137238f5f99ea980
Agata Opalach <aopalach@netflix.com>
2019-12-09 12:06:50 -0800

    REMOVE BEFORE MERGING define DEFAULT flag for tools on/off

--- Source/API/EbSvtAv1Enc.h
+++ Source/API/EbSvtAv1Enc.h
@@ -26,6 +26,8 @@ extern "C" {
 
 #define MAX_ENC_PRESET                              8
 
+#define DEFAULT                                    -1
+
 #define EB_BUFFERFLAG_EOS           0x00000001  // signals the last packet of the stream
 #define EB_BUFFERFLAG_SHOW_EXT      0x00000002  // signals that the packet contains a show existing frame at the end
 #define EB_BUFFERFLAG_HAS_TD        0x00000004  // signals that the packet contains a TD
@@ -238,7 +240,7 @@ typedef struct EbSvtAv1EncConfiguration
     /* RDOQ
     *
     * Default is -1. */
-    int8_t                   enable_rdoq;
+    int                      enable_rdoq;
 
     /* Filter intra prediction
     *
